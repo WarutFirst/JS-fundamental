9f45f9183c2010b22ffdb9fa417b92c38baadb17
WarutFirst <firstwarut@gmail.com>
2024-08-02 09:45:25 +0700

    MathObject

---
+++ MathObject.js
@@ -0,0 +1,19 @@
+let x = 0;
+let y = 1;
+let z = 2;
+
+/* z = Math.round(x); */
+/* z = Math.floor(x); */
+/* z = Math.ceil(x); */
+/* z = Math.trunc(x); */
+/* z = Math.pow(y, x); */
+/* z = Math.sqrt(x); */
+/* z = Math.log(x); */
+/* z = Math.cos(x); */
+/* z = Math.tan(x); */
+/* z = Math.abs(x); */
+/* z = Math.sign(x); */
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(min);
